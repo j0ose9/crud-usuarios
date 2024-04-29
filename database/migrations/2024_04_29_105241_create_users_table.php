75ae8734d595e5fb27ceb1681c7166cf08795fdd
jose <trueno1511@gmail.com>
2024-04-29 13:03:19 +0200

    primera migration

--- database/migrations/2024_04_29_105241_create_users_table.php
+++ database/migrations/2024_04_29_105241_create_users_table.php
@@ -13,11 +13,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
+            $table->string('nombre');
             $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
-            $table->rememberToken();
+            $table->enum('rango', ['usuario', 'admin']);
             $table->timestamps();
         });
     }
